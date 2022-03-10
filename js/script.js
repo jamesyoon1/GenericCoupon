7ad40a1d43af12579f0d29f7e6096b680ecbc0ee
James Yoon <65934713+jamesyoon1@users.noreply.github.com>
2022-03-10 16:33:20 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,39 +1,60 @@
-function clickTest() {
+function generateJSON() {
+  //new business object
+  var b = new Object();
+  b.name = document.getElementById("enterName").value;
+  b.address = document.getElementById("enterAddress").value;
+  b.description = document.getElementById("enterDescription").value;
+  b.highlight = document.getElementById("enterHighlight").value;
+  b.aboutThisDeal = document.getElementById("enterAboutThisDeal").value;
+  b.finePrint = document.getElementById("enterFinePrint").value;
+  b.about = document.getElementById("enterAbout").value;
 
-    var business = new Object();
-    business.name = document.getElementById("name").value;
-    business.address = document.getElementById("address").value;
-    business.description = document.getElementById("description").value;
-    business.highlight = document.getElementById("highlight").value;
-    business.aboutThisDeal = document.getElementById("aboutThisDeal").value;
-    business.finePrint = document.getElementById("finePrint").value;
-    business.about = document.getElementById("about").value;
+  //option 1 details
+  b.option1 = document.getElementById("enterOption1").value;
+  b.option1Regular = document.getElementById("enterOption1Regular").value;
+  b.option1Discounted = document.getElementById("enterOption1Discounted").value;
+  var discount = parseFloat(b.option1Regular) - parseFloat(b.option1Discounted);
+  b.option1DiscountPercentage = Math.round(
+    (discount / parseFloat(b.option1Regular)) * 100
+  ).toString();
 
-    business.option1 = document.getElementById("option1").value;
-    business.option1Regular = document.getElementById("option1Regular").value;
-    business.option1Discounted = document.getElementById("option1Discounted").value;
-    var discount = (parseFloat(business.option1Regular) - parseFloat(business.option1Discounted));
-    business.option1DiscountPercentage = Math.round((discount / (parseFloat(business.option1Regular))) * 100);
+  //option 2 details
+  b.option2 = document.getElementById("option2").value;
+  b.option2Regular = document.getElementById("option2Regular").value;
+  b.option2Discounted = document.getElementById("option2Discounted").value;
+  discount = parseFloat(b.option2Regular) - parseFloat(b.option2Discounted);
+  b.option2DiscountPercentage = Math.round(
+    (discount / parseFloat(b.option2Regular)) * 100
+  ).toString();
 
-    business.option2 = document.getElementById("option2").value;
-    business.option2Regular = document.getElementById("option2Regular").value;
-    business.option2Discounted = document.getElementById("option2Discounted").value;
-    discount = (parseFloat(business.option2Regular) - parseFloat(business.option2Discounted));
-    business.option2DiscountPercentage = Math.round((discount / (parseFloat(business.option2Regular))) * 100);
+  //option 3 details
+  b.option3 = document.getElementById("option3").value;
+  b.option3Regular = document.getElementById("option3Regular").value;
+  b.option3Discounted = document.getElementById("option3Discounted").value;
+  discount = parseFloat(b.option3Regular) - parseFloat(b.option3Discounted);
+  b.option3DiscountPercentage = Math.round(
+    (discount / parseFloat(b.option3Regular)) * 100
+  ).toString();
 
-    business.option3 = document.getElementById("option3").value;
-    business.option3Regular = document.getElementById("option3Regular").value;
-    business.option3Discounted = document.getElementById("option3Discounted").value;
-    discount = (parseFloat(business.option3Regular) - parseFloat(business.option3Discounted));
-    business.option3DiscountPercentage = Math.round((discount / (parseFloat(business.option3Regular))) * 100);
+  var jsonStrings = JSON.stringify(b);
 
-    var jsonStrings = JSON.stringify(business);
-
-    const FileSystem = require("fs");
-    FileSystem.writeFile('file.json', JSON.stringify(business), (error) => {
-       if (error) throw error;
-     });
+  window.alert(jsonStrings);
+  exportJSON("b.json", jsonStrings);
 }
 
+function exportJSON(filename, text) {
+  var element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
 
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
 
